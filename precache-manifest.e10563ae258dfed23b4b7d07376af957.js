0212c9f7e31b6b26f48e9ea247ea11d8221561f7
fatemaowedah <fatema.owedah@gmail.com>
2020-06-30 04:40:25 +0300

    Updates

---
+++ precache-manifest.e10563ae258dfed23b4b7d07376af957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5809edc465a3aa2172b9da412aae16f",
+    "url": "/RESTY27/index.html"
+  },
+  {
+    "revision": "d9c3cce035e185590094",
+    "url": "/RESTY27/static/css/main.deb73a8a.chunk.css"
+  },
+  {
+    "revision": "0828aecb8d1951d6e3e4",
+    "url": "/RESTY27/static/js/2.e37d5b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTY27/static/js/2.e37d5b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c3cce035e185590094",
+    "url": "/RESTY27/static/js/main.44040e3a.chunk.js"
+  },
+  {
+    "revision": "ad8f801bce132eb55c2b",
+    "url": "/RESTY27/static/js/runtime-main.858848d2.js"
+  }
+]);
