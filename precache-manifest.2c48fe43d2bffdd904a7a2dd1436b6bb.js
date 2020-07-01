71cb8f2a6c972d3ba6093787343239ecc989f68e
fatemaowedah <runner@fv-az70.khv541grv2tufetzdw34snjqfe.bx.internal.cloudapp.net>
2020-07-01 07:45:50 +0000

    Updates

---
+++ precache-manifest.2c48fe43d2bffdd904a7a2dd1436b6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814bc0b3c54cbb32592ff912cce21590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f357f09974d106fbfa",
+    "url": "/static/css/main.929e6ba2.chunk.css"
+  },
+  {
+    "revision": "82864f32bc6f2c19ae13",
+    "url": "/static/js/2.7e60eb48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e60eb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f357f09974d106fbfa",
+    "url": "/static/js/main.fed051b5.chunk.js"
+  },
+  {
+    "revision": "85908897e7e63c0c9ac9",
+    "url": "/static/js/runtime-main.9513d861.js"
+  }
+]);
