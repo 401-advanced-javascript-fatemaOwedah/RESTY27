ddc974820abfa09fe2c3c271115cafc88ead9ef1
fatemaowedah <fatema.owedah@gmail.com>
2020-07-02 07:12:17 +0300

    add

--- src/__tests__/components/header.test.js
+++ src/__tests__/components/header.test.js
@@ -9,9 +9,5 @@ describe('<Header/>', ()=> {
         let head = shallow(<Header/>);
         expect(head.find('h1').exists()).toBeTruthy();
     });
-    it('renders correctly', ()=> {
-        const tree = renderer.create(<Header/>).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
 
 })
